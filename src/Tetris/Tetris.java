2a7244ccbd2a4378ac95f3af9756a52ace9f9ee7
TheFourthHorse <thefourthhorseman3@gmail.com>
2016-12-28 14:55:53 -0500

    Added a file

--- src/Tetris/Tetris.java
+++ src/Tetris/Tetris.java
@@ -29,6 +29,7 @@ public class Tetris extends Applet {
         int totalTrappedSpaces;
         int SessionNo = 1;
         Data D = new Data();
+		FineData D = new FineData();
         boolean OutputTheDataWhenFirstFast;
         public static Frame frame;
         private final static int INITIAL_DELAY = 1000;
