c80ac11de7594dc9d0bf4825abc59548e9a30a08
Kevin O'Neill <koneill1994@gmail.com>
2017-11-09 15:32:12 -0500

    Some work on making vanilla abt

--- src/LDS/AttentionalBlinkTask.java
+++ src/LDS/AttentionalBlinkTask.java
@@ -48,16 +48,144 @@ public class AttentionalBlinkTask {
 
     int Game;
 
+    int BlockNo = 0;
+
     char Tab = ',';
 
     boolean FirstTime = true;
 
     boolean StartTimerNow;
     Task B = null;
+    
+    
+    String TargetString = "";
+    char TargetLetter = (char)0;
+    int TargetIndex;
+
 
     public AttentionalBlinkTask(Task B, int NoOfBlocks){
     
+        //reset timers
+        for (int i = 0; i < 5; i++) StopTime[i] = -1;
     
+        BlockNo = B.TaskCounter;
+        
+        B.TaskCounter++;
+        
+        W("MADE D");
+        W("BlockNo="+BlockNo);
+
     
     }
+    
+    
+    public void W(String S) {
+        System.out.println(S);
+    }
+    
+    public void StartTimer(int TimeMode) {
+        StartTime[TimeMode] = CT.currentTimeMillis();
+    } 
+  
+    public void StopTimer(int TimeMode) {
+        StopTime[TimeMode] = CT.currentTimeMillis() - StartTime[TimeMode];
+    } 
+    
+    public static String ReplaceCharAt(String str, int pos, char c) {
+        //char c = character.charAt(0);
+        StringBuffer buf = new StringBuffer(str);
+        buf.setCharAt(pos-1, c);
+        return buf.toString();
+    }
+    
+        public void GetQue(int ImageNo) {
+        
+        
+        //build TargetString of numbers
+        
+        TargetString = "";
+        
+        int Digit = 0;
+        int NewDigit;
+            
+        for (int i = 0; i < B.NoOfSymbols; i++) {
+            
+            if (i == 0)
+                
+                Digit = R.nextInt(10);
+            
+            else {
+                
+                do {
+                    NewDigit = R.nextInt(10);
+                } while (Digit == NewDigit);
+                
+                Digit = NewDigit;
+                
+            }
+                
+            TargetString += "" + Digit;
+            
+        }
+        
+        TargetLetter = Task.FirstLettersUsed.charAt(R.nextInt(Task.FirstLettersUsed.length()));
+        
+//        do {
+//            FirstTargetIndex = R.nextInt(B.NoOfSymbols);
+//        } while ((FirstTargetIndex < Task.FirstLetterIndex[0]) | (FirstTargetIndex > Task.FirstLetterIndex[1]));
+        
+        int Distance = R.nextInt(Task.LetterIndex[1] - Task.LetterIndex[0]) + 1; //get a number from 1 to 13
+        
+        int LastPossibleStartingPosition = Task.LetterIndex[1] - Task.LetterIndex[0] - Distance;
+        
+        if (LastPossibleStartingPosition == 0)
+            TargetIndex = 1;
+        else
+            TargetIndex = 1 + R.nextInt(LastPossibleStartingPosition + 1);
+        
+        TargetIndex += Task.LetterIndex[0] - 1;
+        
+        TargetString = ReplaceCharAt(TargetString, TargetIndex, TargetLetter);
+        
+        SecondTargetIndex = 0;
+        SecondTargetLetter = '*';
+        SecondLetterIsPresentInTarget = true;
+        
+        //one in three chance there is no target
+        if (R.nextInt(3) == 0) SecondLetterIsPresentInTarget = false;
+        
+        FirstSecondLetterStr = "NO_SECOND_LETTER";
+        FirstTargetLetterMemory = FirstTargetLetter;
+            
+        if (SecondLetterIsPresentInTarget) {
+        
+            do {
+                SecondTargetLetter = Task.SecondLettersUsed.charAt(R.nextInt(Task.SecondLettersUsed.length()));
+            } while (FirstTargetLetter == SecondTargetLetter);
+
+//            do {
+//                SecondTargetIndex = R.nextInt(B.NoOfSymbols);
+//            } while ((SecondTargetIndex < Task.SecondLetterIndex[0]) | (SecondTargetIndex > Task.SecondLetterIndex[1]));
+
+            SecondTargetIndex = FirstTargetIndex + Distance;
+            
+            TargetString = ReplaceCharAt(TargetString, SecondTargetIndex, SecondTargetLetter);
+
+            SecondTargetLetterMemory = SecondTargetLetter;
+            
+            if (SecondTargetLetter < FirstTargetLetter) {
+                CorrectAnswerStr = "LEFT";
+                FirstSecondLetterStr = "SECOND_LETTER_FIRST";
+            }
+        
+            if (SecondTargetLetter > FirstTargetLetter) {
+                CorrectAnswerStr = "RIGHT";
+                FirstSecondLetterStr = "FIRST_LETTER_FIRST";
+            }
+            
+        } else CorrectAnswerStr = "MIDDLE";
+                
+    }
+    
+    
 }
